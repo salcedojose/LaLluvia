1312b53398c0e21da1c04ab000e7c7d3fe8966fc
Salcedo Jose <josemasal@gmail.com>
2017-05-22 12:20:14 -0700

    Sketch de Arduino listo para enviar datos en formato Json.

--- Proyecto/arduino/SistemaRiego/SistemaRiego.ino
+++ Proyecto/arduino/SistemaRiego/SistemaRiego.ino
@@ -1,10 +1,39 @@
+// CONSTANTES.
+// Pines para los distintos valores.
+const byte PIN_LUZ = 2;
+const byte PIN_TEMPERATURA = 3;
+const byte PIN_PH = 4;
+const byte PIN_HUMEDAD = 5;
+
 // PREPARACION.
 void setup() {
+  // Preparar comunicacion serial.
   Serial.begin(9600);
+
+  // Preparar valores de entrada.
+  pinMode(PIN_LUZ, INPUT);
+  pinMode(PIN_TEMPERATURA, INPUT);
+  pinMode(PIN_PH, INPUT);
+  pinMode(PIN_HUMEDAD, INPUT);
 }
 
 // PRINCIPAL.
 void loop() {
-  Serial.println("Hello World");
+  // Leer valores.
+  const int luz = analogRead(PIN_LUZ);
+  const int temperatura = analogRead(PIN_TEMPERATURA);
+  const int ph = analogRead(PIN_PH);
+  const int humedad = analogRead(PIN_HUMEDAD);
+
+  // Guardar datos obtenidos en formato Json.
+  String datosJson = "{";
+  datosJson += "\"luz\": " + String(luz);
+  datosJson += ", \"temperatura\": " + String(temperatura);
+  datosJson += ", \"ph\": " + String(ph);
+  datosJson += ", \"humedad\": " + String(humedad);
+  datosJson += "}";
+
+  // Desplegar en serial los datos obtenidos.
+  Serial.println(datosJson);
   delay(1000);
 }
